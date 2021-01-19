51ba373ae6ef1f16b2299258164b09bbe125e5c8
Sam Ferebee <sam.ferebee@gmail.com>
2021-01-19 11:12:34 -0500

    suh

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,22 @@
 class Animal
 
+    
+    @@all = []
+    attr_accessor :name, :zoo, :origin
+    attr_reader :species
+
+    def initialize(species, name)
+        @species = species
+        @name = name
+        self.class.all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        all.clear
+    end
+
 end
--- app/models/origin.rb
+++ app/models/origin.rb
@@ -1,3 +1,19 @@
 class Origin
 
+    @@all = []
+    attr_accessor :continent, :country
+
+    def initialize(continent, country)
+        @continent = continent
+        @country = country
+        self.class.all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.animals
+        
+
 end
